9122dc41c5d30a85a8ef78b643489f90bf5c2b16
bluepongo <563091001@qq.com>
2021-03-28 23:29:52 +0800

    LC173

---
+++ LC173/main.go
@@ -0,0 +1,35 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+type BSTIterator struct {
+	arr []int
+}
+
+func Constructor(root *TreeNode) (it BSTIterator) {
+	it.inorder(root)
+	return
+}
+
+func (it *BSTIterator) inorder(node *TreeNode) {
+	if node == nil {
+		return
+	}
+	it.inorder(node.Left)
+	it.arr = append(it.arr, node.Val)
+	it.inorder(node.Right)
+}
+
+func (it *BSTIterator) Next() int {
+	val := it.arr[0]
+	it.arr = it.arr[1:]
+	return val
+}
+
+func (it *BSTIterator) HasNext() bool {
+	return len(it.arr) > 0
+}
