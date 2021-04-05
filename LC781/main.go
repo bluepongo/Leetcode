a9faa8645ccb9f03d65539bc9553be62a99170cb
bluepongo <563091001@qq.com>
2021-04-05 08:53:29 +0800

    LC088

--- LC781/main.go
+++ LC781/main.go
@@ -1,1 +1,12 @@
-package LC781
+package main
+
+func numRabbits(answers []int) (ans int) {
+	count := map[int]int{}
+	for _, y := range answers {
+		count[y]++
+	}
+	for y, x := range count {
+		ans += (x + y) / (y + 1) * (y + 1)
+	}
+	return
+}
