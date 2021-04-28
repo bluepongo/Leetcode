e120a43af74c71a05e8dc03bbbb38b473ffe55b9
bluepongo <563091001@qq.com>
2021-04-28 08:55:38 +0800

    LC633

---
+++ LC633/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "math"
+
+func judgeSquareSum(c int) bool {
+	for a := 0; a*a <= c; a++ {
+		rt := math.Sqrt(float64(c - a*a))
+		if rt == math.Floor(rt) {
+			return true
+		}
+	}
+	return false
+}
