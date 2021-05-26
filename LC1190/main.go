de71b42ab2efac96b4e752cf964ef287cb4ea339
bluepongo <563091001@qq.com>
2021-05-26 08:58:37 +0800

    LC1190

---
+++ LC1190/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func reverseParentheses(s string) string {
+	stack := [][]byte{}
+	str := []byte{}
+	for i := range s {
+		if s[i] == '(' {
+			stack = append(stack, str)
+			str = []byte{}
+		} else if s[i] == ')' {
+			for j, n := 0, len(str); j < n/2; j++ {
+				str[j], str[n-1-j] = str[n-1-j], str[j]
+			}
+			str = append(stack[len(stack)-1], str...)
+			stack = stack[:len(stack)-1]
+		} else {
+			str = append(str, s[i])
+		}
+	}
+	return string(str)
+}
+
+func main() {
+	s := "(i(love)u)"
+	res := reverseParentheses(s)
+	fmt.Println(res)
+}
