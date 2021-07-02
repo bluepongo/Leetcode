e7fc551bfdbe58e1e4ee1591753f0a4cc40bd8c2
bluepongo <563091001@qq.com>
2021-07-02 09:40:49 +0800

    LC1833

---
+++ LC1833/main.go
@@ -0,0 +1,13 @@
+package main
+
+func maxIceCream(costs []int, coins int) (ans int) {
+	sort.Ints(costs)
+	for _, c := range costs {
+		if coins < c {
+			break
+		}
+		coins -= c
+		ans++
+	}
+	return
+}
