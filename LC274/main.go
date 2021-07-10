3149055f940da03fad1eb0bfb0c9b3ffd06ee84b
bluepongo <563091001@qq.com>
2021-07-11 01:09:23 +0800

    LC274

---
+++ LC274/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "sort"
+
+func hIndex(citations []int) (h int) {
+	sort.Ints(citations)
+	for i := len(citations) - 1; i >= 0 && citations[i] > h; i-- {
+		h++
+	}
+	return
+}
