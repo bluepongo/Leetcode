e06c11bc5146969d547dc194719611d069286740
bluepongo <563091001@qq.com>
2021-08-17 09:02:25 +0800

    LC551

---
+++ LC551/main.go
@@ -0,0 +1,22 @@
+package main
+
+func checkRecord(s string) bool {
+	absents, lates := 0, 0
+	for _, ch := range s {
+		if ch == 'A' {
+			absents++
+			if absents >= 2 {
+				return false
+			}
+		}
+		if ch == 'L' {
+			lates++
+			if lates >= 3 {
+				return false
+			}
+		} else {
+			lates = 0
+		}
+	}
+	return true
+}
