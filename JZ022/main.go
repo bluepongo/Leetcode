ca9edcadc6415acb31ee921be1d1a7b175b8177f
bluepongo <563091001@qq.com>
2021-09-02 00:15:52 +0800

    JZ022

--- JZ022/main.go
+++ JZ022/main.go
@@ -1,5 +1,10 @@
 package main
 
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
 func getKthFromEnd(head *ListNode, k int) (kth *ListNode) {
 	n := 0
 	for node := head; node != nil; node = node.Next {
