ebe26e68e3b1c7e1d8212ff0335928f9ed215b90
bluepongo <563091001@qq.com>
2021-09-02 00:15:14 +0800

    JZ022

---
+++ JZ022/main.go
@@ -0,0 +1,12 @@
+package main
+
+func getKthFromEnd(head *ListNode, k int) (kth *ListNode) {
+	n := 0
+	for node := head; node != nil; node = node.Next {
+		n++
+	}
+	for kth = head; n > k; n-- {
+		kth = kth.Next
+	}
+	return
+}
