eafa74e4ad3a813a5634b587f024a85d6b97a567
bluepongo <563091001@qq.com>
2021-09-07 00:34:18 +0800

    LC1221

---
+++ LC1221/main.go
@@ -0,0 +1,16 @@
+package main
+
+func balancedStringSplit(s string) (ans int) {
+	d := 0
+	for _, ch := range s {
+		if ch == 'L' {
+			d++
+		} else {
+			d--
+		}
+		if d == 0 {
+			ans++
+		}
+	}
+	return
+}
