afb230d0044958006cfe3c8fd7c5362126cef376
bluepongo <563091001@qq.com>
2021-09-10 00:43:40 +0800

    LC1894

---
+++ LC1894/main.go
@@ -0,0 +1,16 @@
+package main
+
+func chalkReplacer(chalk []int, k int) int {
+	total := 0
+	for _, v := range chalk {
+		total += v
+	}
+	k %= total
+	for i, c := range chalk {
+		if k < c {
+			return i
+		}
+		k -= c
+	}
+	return 0
+}
