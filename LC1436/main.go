79c1c6e96493f470173ac4559085b4853f2432ea
bluepongo <563091001@qq.com>
2021-10-01 21:39:13 +0800

    LC1436

---
+++ LC1436/main.go
@@ -0,0 +1,14 @@
+package main
+
+func destCity(paths [][]string) string {
+	citiesA := map[string]bool{}
+	for _, path := range paths {
+		citiesA[path[0]] = true
+	}
+	for _, path := range paths {
+		if !citiesA[path[1]] {
+			return path[1]
+		}
+	}
+	return ""
+}
