47ec906b7054627edb6c9f6b194461daab0a0c00
bluepongo <563091001@qq.com>
2021-10-02 11:23:13 +0800

    LC405

---
+++ LC405/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "strings"
+
+func toHex(num int) string {
+	if num == 0 {
+		return "0"
+	}
+	sb := &strings.Builder{}
+	for i := 7; i >= 0; i-- {
+		val := num >> (4 * i) & 0xf
+		if val > 0 || sb.Len() > 0 {
+			var digit byte
+			if val < 10 {
+				digit = '0' + byte(val)
+			} else {
+				digit = 'a' + byte(val-10)
+			}
+			sb.WriteByte(digit)
+		}
+	}
+	return sb.String()
+}
