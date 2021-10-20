36d8ab210d74528343cc4942bfcd8193ca80314b
bluepongo <563091001@qq.com>
2021-10-20 09:37:00 +0800

    LC211

---
+++ LC453/main.go
@@ -0,0 +1,14 @@
+package main
+
+func minMoves(nums []int) (ans int) {
+	min := nums[0]
+	for _, num := range nums[1:] {
+		if num < min {
+			min = num
+		}
+	}
+	for _, num := range nums {
+		ans += num - min
+	}
+	return
+}
