5debc6f35aca5429a21e680185ea6a38edfacae7
bluepongo <563091001@qq.com>
2021-10-25 09:12:20 +0800

    LC240

---
+++ LC240/main.go
@@ -0,0 +1,12 @@
+package main
+
+func searchMatrix(matrix [][]int, target int) bool {
+	for _, row := range matrix {
+		for _, v := range row {
+			if v == target {
+				return true
+			}
+		}
+	}
+	return false
+}
