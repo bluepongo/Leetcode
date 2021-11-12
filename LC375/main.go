e45f59e21c752be89e4fd0aba6a4af3b1bdadb18
bluepongo <563091001@qq.com>
2021-11-12 09:15:02 +0800

    LC375

---
+++ LC375/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "math"
+
+func getMoneyAmount(n int) int {
+	f := make([][]int, n+1)
+	for i := range f {
+		f[i] = make([]int, n+1)
+	}
+	for i := n - 1; i >= 1; i-- {
+		for j := i + 1; j <= n; j++ {
+			minCost := math.MaxInt32
+			for k := i; k < j; k++ {
+				cost := k + max(f[i][k-1], f[k+1][j])
+				if cost < minCost {
+					minCost = cost
+				}
+			}
+			f[i][j] = minCost
+		}
+	}
+	return f[1][n]
+}
+
+func max(a, b int) int {
+	if b > a {
+		return b
+	}
+	return a
+}
