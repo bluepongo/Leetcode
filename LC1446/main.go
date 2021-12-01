9c318b386f4a770217a7afbc9b4d707d03043615
bluepongo <563091001@qq.com>
2021-12-01 09:54:38 +0800

    LC1446

---
+++ LC1446/main.go
@@ -0,0 +1,16 @@
+package main
+
+func maxPower(s string) int {
+	ans, cnt := 1, 1
+	for i := 1; i < len(s); i++ {
+		if s[i] == s[i-1] {
+			cnt++
+			if cnt > ans {
+				ans = cnt
+			}
+		} else {
+			cnt = 1
+		}
+	}
+	return ans
+}
