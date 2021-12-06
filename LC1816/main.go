179146eea41258d1b922596eaa85a7f6daf89778
bluepongo <563091001@qq.com>
2021-12-06 09:34:53 +0800

    LC1816

---
+++ LC1816/main.go
@@ -0,0 +1,15 @@
+package main
+
+func truncateSentence(s string, k int) string {
+	n, end, count := len(s), 0, 0
+	for i := 1; i <= n; i++ {
+		if i == n || s[i] == ' ' {
+			count++
+			if count == k {
+				end = i
+				break
+			}
+		}
+	}
+	return s[:end]
+}
