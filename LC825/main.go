bbf21e66d2456f4925beb62bd7874f6deef1c839
bluepongo <563091001@qq.com>
2021-12-27 09:17:26 +0800

    LC825

---
+++ LC825/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "sort"
+
+func numFriendRequests(ages []int) (ans int) {
+	sort.Ints(ages)
+	left, right := 0, 0
+	for _, age := range ages {
+		if age < 15 {
+			continue
+		}
+		for ages[left]*2 <= age+14 {
+			left++
+		}
+		for right+1 < len(ages) && ages[right+1] <= age {
+			right++
+		}
+		ans += right - left
+	}
+	return
+}
