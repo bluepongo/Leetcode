a117e2a6c499a60219998f8074b791acaa9448b7
bluepongo <563091001@qq.com>
2022-01-15 13:40:32 +0800

    LC1716

---
+++ LC1716/main.go
@@ -0,0 +1,14 @@
+package main
+
+func totalMoney(n int) (ans int) {
+	week, day := 1, 1
+	for i := 0; i < n; i++ {
+		ans += week + day - 1
+		day++
+		if day == 8 {
+			day = 1
+			week++
+		}
+	}
+	return
+}
