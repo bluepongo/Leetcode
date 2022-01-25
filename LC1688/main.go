3dfdc89b0bd565f9341aeb481eef6b1a82c0f8b6
bluepongo <563091001@qq.com>
2022-01-25 09:23:53 +0800

    LC1688

---
+++ LC1688/main.go
@@ -0,0 +1,15 @@
+package main
+
+func numberOfMatches(n int) int {
+	ans := 0
+	for n > 1 {
+		if n%2 == 0 {
+			ans += n / 2
+			n /= 2
+		} else {
+			ans += (n - 1) / 2
+			n = (n-1)/2 + 1
+		}
+	}
+	return ans
+}
