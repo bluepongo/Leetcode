f7f60c356974df49dc96511e6cf12185cbf2cf3e
bluepongo <563091001@qq.com>
2022-01-26 09:30:40 +0800

    LC2013

---
+++ LC2013/main.go
@@ -0,0 +1,32 @@
+package main
+
+type DetectSquares map[int]map[int]int
+
+func Constructor() DetectSquares {
+	return DetectSquares{}
+}
+
+func (s DetectSquares) Add(point []int) {
+	x, y := point[0], point[1]
+	if s[y] == nil {
+		s[y] = map[int]int{}
+	}
+	s[y][x]++
+}
+
+func (s DetectSquares) Count(point []int) (ans int) {
+	x, y := point[0], point[1]
+	if s[y] == nil {
+		return
+	}
+	yCnt := s[y]
+	for col, colCnt := range s {
+		if col != y {
+			// 根据对称性，这里可以不用取绝对值
+			d := col - y
+			ans += colCnt[x] * yCnt[x+d] * colCnt[x+d]
+			ans += colCnt[x] * yCnt[x-d] * colCnt[x-d]
+		}
+	}
+	return
+}
