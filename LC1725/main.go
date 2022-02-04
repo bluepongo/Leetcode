a4d4b51ff76ebe672656657dcb10eeedc224dca1
bluepongo <563091001@qq.com>
2022-02-04 17:46:14 +0800

    LC1725

---
+++ LC1725/main.go
@@ -0,0 +1,21 @@
+package main
+
+func countGoodRectangles(rectangles [][]int) (ans int) {
+	maxLen := 0
+	for _, rect := range rectangles {
+		k := min(rect[0], rect[1])
+		if k == maxLen {
+			ans++
+		} else if k > maxLen {
+			maxLen, ans = k, 1
+		}
+	}
+	return
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
