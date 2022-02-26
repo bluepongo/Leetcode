c22bf61d4a534629d478c21d91b6fcddf85bd32e
bluepongo <563091001@qq.com>
2022-02-26 23:30:44 +0800

    LC2016

---
+++ LC2016/main.go
@@ -0,0 +1,20 @@
+package main
+
+func maximumDifference(nums []int) int {
+	ans := -1
+	for i, preMin := 1, nums[0]; i < len(nums); i++ {
+		if nums[i] > preMin {
+			ans = max(ans, nums[i]-preMin)
+		} else {
+			preMin = nums[i]
+		}
+	}
+	return ans
+}
+
+func max(a, b int) int {
+	if b > a {
+		return b
+	}
+	return a
+}
