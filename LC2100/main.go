d4ab9502b7a21cfab8af3577e2e580393046901b
bluepongo <563091001@qq.com>
2022-03-06 20:52:47 +0800

    LC2100

---
+++ LC2100/main.go
@@ -0,0 +1,22 @@
+package main
+
+func goodDaysToRobBank(security []int, time int) (ans []int) {
+	n := len(security)
+	left := make([]int, n)
+	right := make([]int, n)
+	for i := 1; i < n; i++ {
+		if security[i] <= security[i-1] {
+			left[i] = left[i-1] + 1
+		}
+		if security[n-i-1] <= security[n-i] {
+			right[n-i-1] = right[n-i] + 1
+		}
+	}
+
+	for i := time; i < n-time; i++ {
+		if left[i] >= time && right[i] >= time {
+			ans = append(ans, i)
+		}
+	}
+	return
+}
