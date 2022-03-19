45bcdb7276c290c9296cc6f4e2742c48d997c7a1
bluepongo <563091001@qq.com>
2022-03-19 13:33:51 +0800

    LC606

---
+++ LC606/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func tree2str(root *TreeNode) string {
+	switch {
+	case root == nil:
+		return ""
+	case root.Left == nil && root.Right == nil:
+		return strconv.Itoa(root.Val)
+	case root.Right == nil:
+		return fmt.Sprintf("%d(%s)", root.Val, tree2str(root.Left))
+	default:
+		return fmt.Sprintf("%d(%s)(%s)", root.Val, tree2str(root.Left), tree2str(root.Right))
+	}
+}
