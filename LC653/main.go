2928111649330a5d738184a63c0796cbfc8dd74b
bluepongo <563091001@qq.com>
2022-03-21 11:01:29 +0800

    LC653

---
+++ LC653/main.go
@@ -0,0 +1,17 @@
+package main
+
+func findTarget(root *TreeNode, k int) bool {
+	set := map[int]struct{}{}
+	var dfs func(*TreeNode) bool
+	dfs = func(node *TreeNode) bool {
+		if node == nil {
+			return false
+		}
+		if _, ok := set[k-node.Val]; ok {
+			return true
+		}
+		set[node.Val] = struct{}{}
+		return dfs(node.Left) || dfs(node.Right)
+	}
+	return dfs(root)
+}
