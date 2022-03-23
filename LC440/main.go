ecd38701b83c31b6c8f61416f9ef4cc74b7ff614
bluepongo <563091001@qq.com>
2022-03-23 14:31:52 +0800

    LC440

---
+++ LC440/main.go
@@ -0,0 +1,34 @@
+package main
+
+func getSteps(cur, n int) (steps int) {
+	first, last := cur, cur
+	for first <= n {
+		steps += min(last, n) - first + 1
+		first *= 10
+		last = last*10 + 9
+	}
+	return
+}
+
+func findKthNumber(n, k int) int {
+	cur := 1
+	k--
+	for k > 0 {
+		steps := getSteps(cur, n)
+		if steps <= k {
+			k -= steps
+			cur++
+		} else {
+			cur *= 10
+			k--
+		}
+	}
+	return cur
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
